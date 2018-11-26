28dd9249dcfeff2c1c0eac7733ca047c04ff1133
Rafael Rosenzvaig <rafaelvr@al.insper.edu.br>
2018-11-26 17:37:46 -0200

    juntando com o lei

--- src/Dash.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Dash from './Dash';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Dash />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
