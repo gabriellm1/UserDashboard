01de5a05c97e9cd45c4fb278a83a91668106094d
guipleite <guipleite@gmail.com>
2018-11-26 16:31:06 -0200

    Front com Scroll

--- src/Dash.js
+++ src/Dash.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './Dash.css';
 import Chart from './Chart';
+import { Link, Element , Events, animateScroll as scroller } from 'react-scroll'
+
 
 class Dash extends Component {
 
@@ -29,45 +31,65 @@ class Dash extends Component {
     }
   }
 
+  componentDidMount() {
 
-
-
-  render() {
-    return (
-     
-      <div class="graphContainer">
-        <div class="btn-group">
-          <a href="#graphGeral"><button class="tablinks" >Colheita Total</button></a>
-          <a href="#graphEsp_1"><button class="tablinks" >Temporadas de Colheita</button></a>
-          <a href="#graphEsp_2"><button class="tablinks" >Mais alguma coisa</button></a>
-        </div>
-      
-      
-        <div id="graphGeral" class="tabcontent">
-        <h3>Colheita Total</h3><br/>
-    
-    
-    </div>
-      
-      <div id="graphEsp_1" class="tabcontent">
-        <h3>Temporadas de Colheita</h3><br/>
-        <div class="graphs">
-        <p>Grafico vai aqui</p>
-      </div>
-
-      </div>
-      
-      <div id="graphEsp_2" class="tabcontent">
-        <h3>Mais alguma coisa</h3><br/>
-        <div class="graphs">
-        <p>Grafico vai aqui</p>
-      </div>
-
-      </div>
-    </div>
-
-    );
+    Events.scrollEvent.register('begin', function() {
+      console.log("begin", arguments);
+    });
+  
+    Events.scrollEvent.register('end', function() {
+      console.log("end", arguments);
+    });
+  
   }
+  
+  scrollTo() {
+    scroller.scrollTo('scroll-to-element', {
+      duration: 800,
+      delay: 0,
+      smooth: 'easeInOutQuart'
+    })
+  }
+  
+  render () {
+    return (
+      <div>
+        <nav className="navbar navbar-default navbar-fixed-top">
+          <div className="container-fluid">
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav">
+  
+                <li><Link activeClass="active" className="GraphGeral" to="GraphGeral" spy={true} smooth={true} duration={500} >Colheita Total</Link></li>
+                <li><Link activeClass="active" className="GraphEsp_1" to="GraphEsp_1" spy={true} smooth={true} duration={500}>Temporadas de Colheita</Link></li>
+                <li><Link activeClass="active" className="GraphEsp_2" to="GraphEsp_2" spy={true} smooth={true} duration={500} >Test 3</Link></li>
+                <li><Link activeClass="active" className="GraphEsp_3" to="GraphEsp_3" spy={true} smooth={true} duration={500}>Mais alguma Coisa</Link></li>
+             
+              </ul>
+            </div>
+          </div>
+        </nav>
+  
+        <Element name="GraphGeral" className="element" >
+        Colheita Total
+        </Element>
+  
+        <Element name="GraphEsp_1" className="element">
+        Temporadas de Colheita
+        </Element>
+  
+        <Element name="GraphEsp_2" className="element">
+          test 3
+        </Element>
+  
+        <Element name="GraphEsp_3" className="element">
+        Mais alguma coisa
+        </Element>
+  
+      </div>
+
+);
 }
+};
+
 
 export default Dash;
